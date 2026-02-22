a1ddadd0235caef41ae26005582b37939ca151a0
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-22 21:09:35 +0000

    feat: setup react query in project

---
+++ src/app/providers/ReactQueryProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode, useMemo } from "react";
+
+export default function ReactQueryProvider({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  const queryClient = useMemo(() => new QueryClient(), []);
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
