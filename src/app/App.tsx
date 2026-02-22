b93608d204ff98919bd5ef61ca2f3b42c0ddba76
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-22 21:16:13 +0000

    chore: wrap providers around children in App file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,14 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
+import ReactQueryProvider from "./providers/ReactQueryProvider";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ReactQueryProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />;
+      </AuthProvider>
+    </ReactQueryProvider>
+  );
 }
