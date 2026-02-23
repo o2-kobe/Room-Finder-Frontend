07be1f71fd629c89054da885a5e6d62885485e69
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-23 21:18:23 +0000

    feat: add loading component to be used for loading states

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { LoaderIcon } from "lucide-react";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <LoaderIcon className="animate-spin text-3xl" />
+      <p>Loading Page .....</p>
+    </div>
+  );
+};
+export default Loading;
