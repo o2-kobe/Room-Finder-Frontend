984b1704c76d5497d5c19b66bfcdad00b17cd80e
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-23 21:10:07 +0000

    feat: redesign listingCard to display differ well hostels and private rentals with hover states

--- src/app/components/ListingCard.tsx
+++ src/app/components/ListingCard.tsx
@@ -1,74 +1,90 @@
-import { useNavigate } from "react-router";
-import { MapPin, Clock } from "lucide-react";
-import { type Listing } from "../types";
+import { MapPin, Clock, Building2, Home } from "lucide-react";
 import { StatusBadge } from "./StatusBadge";
 import { formatDistanceToNow } from "date-fns";
+import type { ListingDocument } from "../Types/listing";
 
 interface ListingCardProps {
-  listing: Listing;
+  listing: ListingDocument;
   onClick?: () => void;
 }
 
 export function ListingCard({ listing, onClick }: ListingCardProps) {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    if (onClick) {
-      onClick();
-    } else {
-      const path =
-        listing.type === "hostel"
-          ? `/hostel/${listing.id}`
-          : `/rental/${listing.id}`;
-      navigate(path);
-    }
-  };
-
   return (
     <div
-      onClick={handleClick}
-      className="bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-shadow cursor-pointer"
+      onClick={onClick}
+      className="group bg-white rounded-2xl overflow-hidden shadow-md hover:shadow-lg hover:scale-101 transition-all duration-300 cursor-pointer"
     >
       <div className="relative h-48 overflow-hidden">
         <img
-          src={listing.images[0]}
+          src="room.png"
           alt={listing.title}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
         />
         <div className="absolute top-3 right-3">
-          <StatusBadge status={listing.availability} size="sm" />
+          <StatusBadge status={listing.availabilityStatus} size="sm" />
         </div>
-        {listing.type === "hostel" && (
-          <div className="absolute top-3 left-3 bg-primary/90 text-primary-foreground px-2 py-1 rounded-lg text-xs">
-            Hostel
-          </div>
-        )}
       </div>
 
-      <div className="p-4 space-y-2">
-        <h3 className="text-lg line-clamp-1">{listing.title}</h3>
+      <div className="relative">
+        <div className="p-4 space-y-2">
+          <h3 className="text-lg line-clamp-1">{listing.title}</h3>
 
-        <div className="flex items-center gap-1 text-sm text-muted-foreground">
-          <MapPin className="w-4 h-4" />
-          <span className="line-clamp-1">{listing.location.area}</span>
-          {listing.location.distance && (
-            <span className="text-xs">• {listing.location.distance}</span>
+          <div className="flex items-center gap-1 text-sm text-muted-foreground">
+            <MapPin className="w-4 h-4" />
+            <span className="line-clamp-1">{listing.location.area}</span>
+          </div>
+
+          {listing.listingType === "private" && (
+            <div className="text-lg text-accent">
+              GH₵{listing.pricing?.monthlyPrice || "N/A"}
+              <span className="text-sm text-muted-foreground">/month</span>
+            </div>
           )}
+
+          {listing.listingType === "hostel" && (
+            <div className="text-sm text-muted-foreground">
+              From{" "}
+              <span className="text-accent text-lg">
+                GH₵{listing.pricing?.priceRange?.min || "N/A"}
+              </span>{" "}
+              to{" "}
+              <span className="text-accent text-lg">
+                GH₵{listing.pricing?.priceRange?.max || "N/A"}
+              </span>
+            </div>
+          )}
+
+          <div className="flex items-center gap-1 text-xs text-muted-foreground">
+            <Clock className="w-3 h-3" />
+            <span>
+              Updated{" "}
+              {formatDistanceToNow(listing.updatedAt, { addSuffix: true })}
+            </span>
+          </div>
         </div>
 
-        <div className="text-lg text-accent">
-          GH₵ {listing.price || "N/A"}
-          {listing.type === "private" && (
-            <span className="text-sm text-muted-foreground">/month</span>
+        <div className="absolute bottom-[-17px] right-[-20px] px-6 py-5 border rounded-full group-hover:border-gray-400 transition-all duration-300">
+          {listing.listingType === "hostel" ? (
+            <div>
+              <Building2
+                color="#b5baba"
+                size={50}
+                className="mr-3 group-hover:stroke-gray-400 transition-all duration-300"
+              />
+              <p className="text-gray-400">Hostel</p>
+            </div>
+          ) : (
+            <div>
+              <Home
+                color="#b5baba"
+                size={50}
+                className="mr-3 group-hover:stroke-gray-400 transition-all duration-300"
+              />
+              <p className="text-gray-400 group-hover:text-gray-400 transition-all duration-300">
+                Rental
+              </p>
+            </div>
           )}
-        </div>
-
-        <div className="flex items-center gap-1 text-xs text-muted-foreground">
-          <Clock className="w-3 h-3" />
-          <span>
-            Updated{" "}
-            {formatDistanceToNow(listing.lastUpdated, { addSuffix: true })}
-          </span>
         </div>
       </div>
     </div>
