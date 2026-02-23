f7a1e7ac62138313679340e1ed63234a8ecb98c0
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-23 21:13:47 +0000

    feat: alter listing service function to accept right parameters as per controllers

--- src/app/services/apiListings.ts
+++ src/app/services/apiListings.ts
@@ -1,31 +1,51 @@
+import type {
+  ListingDocument,
+  ListingFilters,
+  // UpdateListingType,
+} from "../Types/listing";
 import api from "./axiosInstance";
 
-export async function getListings() {
-  const { data } = await api.get("/listings");
+// Find listings for home and explore pages
+export async function getListings(
+  filters: ListingFilters,
+  cursor?: string,
+  limit: number = 15,
+) {
+  const { data } = await api.get("/listings", {
+    params: {
+      ...filters,
+      cursor,
+      limit,
+    },
+  });
 
   return data;
 }
 
-export async function getMapListings() {
-  const { data } = await api.get("/listings/map");
+// Find listings for map
+export async function getMapListings(filters: ListingFilters) {
+  const { data } = await api.get("/listings/map", {
+    params: filters,
+  });
 
   return data;
 }
 
+//Find single listing
 export async function findListing(listingId: string) {
   const { data } = await api.get(`/listings/${listingId}`);
 
   return data;
 }
 
-export async function createListing(listingData: {}) {
+export async function createListing(listingData: ListingDocument) {
   const { data } = await api.post("/listings/", listingData);
 
   return data;
 }
 
-export async function updateListing(update: {}) {
-  const { data } = await api.patch("/listings/", update);
+export async function updateListing(listingId: string, update: {}) {
+  const { data } = await api.patch(`/listings/${listingId}`, update);
 
   return data;
 }
@@ -37,13 +57,13 @@ export async function deleteListing(listingId: string) {
 }
 
 export async function markListingAsAvailable(listingId: string) {
-  const { data } = await api.patch(`/listings/${listingId}`);
+  const { data } = await api.patch(`/listings/markAvailable/${listingId}`);
 
   return data;
 }
 
 export async function markListingAsInactive(listingId: string) {
-  const { data } = await api.patch(`/listings/${listingId}`);
+  const { data } = await api.patch(`/listings/markInactive/${listingId}`);
 
   return data;
 }
