e2c5c8ce67bd39f3e9516d032f6308c99ca9b95d
Laud Tetteh <kobbykurtx@gmail.com>
2026-02-25 20:53:09 +0000

    style: alter loading to flex col

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -2,9 +2,9 @@ import { LoaderIcon } from "lucide-react";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <LoaderIcon className="animate-spin text-3xl" />
-      <p>Loading Page .....</p>
+      <p>Loading Page</p>
     </div>
   );
 };
